3146557f63f82b7118d0ba59426064c22c30662b
florianleux <florian.leux@gmail.com>
2022-05-24 23:23:58 +0200

    first basic version of positionning

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Tooltip from './components/Tooltip.vue'
+export default {
+  name:"App",
+  components:{Tooltip}
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+
+<Tooltip text="I'm a tooltip"  >
+  <template v-slot:activator="{showTooltip,hideTooltip}">
+    <button @mouseover="showTooltip" @mouseleave="hideTooltip"> Click Me ! </button>
+  </template>
+
+</Tooltip>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="scss">
+@import "./styles/global.scss";
 </style>
--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,14 +1,88 @@
 <template>
+  <span class="relative" >
+    <div
+      v-show="isVisible"
+      class="tooltip absolute whitespace-nowrap"
+      :class="tooltipClass"
+    >
+      {{ text }}
+    </div>
+    <span ref="tooltip-target">
+      <slot name="activator"  :show-tooltip="showTooltip" :hide-tooltip="hideTooltip"/>
+    </span>
+  </span>
+
 
 </template>
 
-<script lang="ts">
+<script>
 export default {
-name: "Tooltip"
+  name: "Tooltip",
+  props: {
+    text: {type: String, required: true}
+  },
+  data: () => {
+    return {
+      isVisible: false,
+      tooltipClass: 'top'
+    }
+  },
+  mounted() {
+    this.calculatePosition();
+
+  },
+  methods: {
+    calculatePosition() {
+      let targetPosition = this.$refs["tooltip-target"].getBoundingClientRect(),
+          windowHeight = window.innerHeight,
+          windowWidth = window.innerWidth;
+
+      if(targetPosition.top > 2*windowHeight/3){
+        this.tooltipClass = 'top'
+      }else if(targetPosition.left < windowWidth/3) {
+      this.tooltipClass = 'right'
+      }else if(targetPosition.right > 2*windowWidth/3) {
+      this.tooltipClass = 'left'
+      }else if(targetPosition.bottom < windowWidth/3) {
+      this.tooltipClass = 'bottom'
+      }
+
+    },
+    hideTooltip() {
+      this.isVisible = false;
+    },
+    showTooltip() {
+      this.isVisible = true;
+
+    }
+
+  }
 
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+$tooltip-margin : -15px;
 
+.tooltip {
+  &.top{
+    top:$tooltip-margin;
+    transform: translate(0%,100%);
+  }
+
+  &.right{
+    right: $tooltip-margin;
+    transform: translate(100%,0);
+  }
+
+  &.left{
+    bottom: $tooltip-margin;
+    transform: translate(0%,-100%);
+  }
+
+  &.bottom{
+    right: $tooltip-margin;
+    transform: translate(-100%,0);
+  }
+}
 </style>
